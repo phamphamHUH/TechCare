d60f6d7a60822200147722e2293a14bb5f2518ec
dyy-11 <randyengrande57@gmail.com>
2026-09-04 17:47:00 +0800

    TEC-101-design: Added deactivate button to UserProfile component

--- frontend/src/users/admin/components/UserManagement/UserProfile.tsx
+++ frontend/src/users/admin/components/UserManagement/UserProfile.tsx
@@ -132,6 +132,13 @@ function UserProfile({
                                         Edit
                                     </button>
 
+                                    <button
+                                        onClick={onBack}
+                                        className="flex h-10 w-10 items-center justify-center rounded-full text-gray-400 transition hover:bg-gray-100 hover:text-gray-900"
+                                    >
+                                        Deactivate
+                                    </button>
+
                                 </div>
                             </div>
 
