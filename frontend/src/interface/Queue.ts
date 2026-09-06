06f281d0999ac528d3deb67561a1621a78e305b1
Marc Jodel L. Dela Cruz <marcjodeldelacruz@gmail.com>
2026-09-06 14:38:39 +0800

    TEC-229-add LaboratoryRequestDetail, LaboratoryItem, and Queue types

---
+++ frontend/src/interface/Queue.ts
@@ -0,0 +1,11 @@
+export type Queue = {
+  id: number;
+  queue_id: string;
+  patient_id: string;
+  queue_number: number;
+  service_id: string;
+  is_priority: boolean;
+  status: string;
+  created_at: string;
+  updated_at: string;
+};
