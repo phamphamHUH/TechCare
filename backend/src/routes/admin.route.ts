e880b5d413e1ead21fc1c115bb3d172ab9af9242
Marc Jodel L. Dela Cruz <marcjodeldelacruz@gmail.com>
2026-09-18 14:49:57 +0800

    TEC-107-feat: Implement addFormTemplates endpoint and update admin routes

--- backend/src/routes/admin.route.ts
+++ backend/src/routes/admin.route.ts
@@ -11,6 +11,7 @@ import {
   addService,
   addActivity,
   addAction,
+  addFormTemplates,
 } from "../controllers/admin/postRequests.controller.js";
 import {
   updateUser,
@@ -21,12 +22,12 @@ import authMiddleware from "../middlewares/auth.middleware.js";
 import adminMiddleware from "../middlewares/admin.middleware.js";
 const router = Router();
 
- //if (ENV.IS_PRODUCTION) {
- //  router.use(authMiddleware, adminMiddleware);
- //  console.log("Lab Staff routes enabled");
- //}
+if (ENV.IS_PRODUCTION) {
+  router.use(authMiddleware, adminMiddleware);
+  console.log("Lab Staff routes enabled");
+}
 
-router.use(authMiddleware, adminMiddleware);
+// router.use(authMiddleware, adminMiddleware);
 
 router.get("/services", getAllservices);
 router.get("/activity", getMyActivities);
@@ -37,6 +38,7 @@ router.post("/add-user", upload.single("image"), addUser);
 router.post("/services", addService);
 router.post("/activities", addActivity);
 router.post("/actions", addAction);
+router.post("/form-templates", addFormTemplates);
 
 router.patch("/users/:user_id", upload.single("image"), updateUser);
 router.patch("/users/:user_id/status", updateUserStatus);
